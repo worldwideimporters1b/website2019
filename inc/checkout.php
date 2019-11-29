1e0f734ea063824ea136d34641ac25794423f410
sander.brandriet <sanderbrandriet@gmail.com>
2019-11-29 11:06:48 +0100

    checkout 1st commit

---
+++ inc/checkout.php
@@ -0,0 +1,44 @@
+<?php
+
+function pay($amount, $orderid, $customerid ){
+
+    $paymentresult = 0;
+
+    // render ideal stuff on website in iframe
+
+    $idealresult = renderIdeal($amount,$shopid,$orderid);
+
+    // check payment
+
+    if($idealresult == 0){
+        $paymentresult; = 0;
+    }
+    if($idealresult == 1){
+        $paymentresult = 1;
+    }
+
+    // return payment results
+
+    return $paymentresult;
+
+}
+
+
+
+renderIdeal($amount,$shopid,$orderid){
+$idealresult = 0;
+
+// do something
+
+return $idealresult;
+}
+
+if($idealresult == 0){
+    $paymentresult = 0;
+}
+if($idealresult == 1){
+    $paymentresult = 1;
+}
+
+
+?>
