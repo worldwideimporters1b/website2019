28cf92d3ed9b9c0aa00c78379d4389e185a35f0b
jornbarkhof <jorn.barkhof@home.nl>
2019-12-13 15:01:56 +0100

    frontend

--- inc/header.php
+++ inc/header.php
@@ -2,12 +2,15 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-3"><img class="flag" src="img/netherlands.png" alt="dutch-flag"> <img class="flag" src="img/united-states.png" alt="united-states-flag">  </div>
-            <div class="col-md-3"></div>
-            <div class="col-md-3"><span class="myaccount">Mijn account</span></div>
-            <div class="col-md-3">
+            <div class="col-md-4"><img class="flag" src="img/netherlands.png" alt="dutch-flag"> <img class="flag"
+                                                                                                     src="img/united-states.png"
+                                                                                                     alt="united-states-flag">
+            </div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4 actionmenu">
                 <span class="totalprice">€ 0,00 </span>
-                <span class="winkelmand">winkelmand</span>
+                <span class="winkelmand"><a class="btn btn-secondary">winkelmand</a></span>
+                <span class="myaccount"><a class="btn btn-secondary">Mijn account</a></span>
             </div>
         </div>
         <div class="row">
@@ -17,21 +20,38 @@
 
 </div>
 <div class="subheader">
-<div class="container">
-    <div class="row">
-        <div class="col-md-3"><a class="btn btn-primary">Home</a>
-            <select class="productgroepselector">
-                <option value="">Selecteer een productgroep</option>
-                <option value="test">test</option>
-                <option value="test">test</option>
-                <option value="test">test</option>
-            </select>
-        </div>
-        <div class="col-md-3">
-            <a class="btn btn-primary">Home</a>
-            <a class="btn btn-primary">Home</a>
-            <a class="btn btn-primary">Home</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="input-group">
+                <span class="input-group-btn">
+                <a class="btn btn-primary">Home</a>
+                </span>
+                <select class="productgroepselector form-control">
+                    <option value="">Selecteer een productgroep</option>
+                    <option value="test">test</option>
+                    <option value="test">test</option>
+                    <option value="test">test</option>
+                </select>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <a class="btn btn-primary">Over ons</a>
+                <a class="btn btn-primary">Contact</a>
+                <a class="btn btn-primary">Chat</a>
+            </div>
+            <div class="col-md-4">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Zoeken">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" type="button">Zoek</button>
+                    </span>
+                </div>
+
+
+            </div>
+
         </div>
     </div>
-</div>
 </div>
