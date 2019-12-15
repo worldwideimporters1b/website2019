b6480706600b7856f5f7a89974f4cda803a66c35
Lennard <s1080997@student.windesheim.nl>
2019-12-15 16:48:50 +0100

    Leipe shit ouwe

--- inc/accountregistreren.php
+++ inc/accountregistreren.php
@@ -9,28 +9,31 @@ function databaseConnectie(){
     return $conn;
 }
 
+//deze functie zal de de registratie functie aanroepen, en controleren of het account is geregistreerd in de database.
 function registreren($gegevens) {
     $conn = databaseConnectie();
-    if (accountregistreren($gegevens["emailadres"], $gegevens["voornaam"],$gegevens["achternaam"], $gegevens["geslacht"],
-            $gegevens["wachtwoord"],$gegevens["adres"],$gegevens["woonplaats"], $gegevens["postcode"], $conn) == 1)
+    if (accountregistreren($gegevens["emailadres"], $gegevens["voornaam"],$gegevens["achternaam"], $gegevens["geslacht"],$gegevens["wachtwoord"],
+            $gegevens["adres"],$gegevens["woonplaats"], $gegevens["postcode"], $gegevens["geboortedatum"], $conn) == 1)
 
-        //controle of het account succesvol is geregistreerd. Wanneer de webshop 1 terug geeft, is het gelukt.
+        /*controle of het account succesvol is geregistreerd. Wanneer de webshop 1 terug geeft, is het gelukt.
+           $result geeft een 1 terug in de functie accountRegistreren, als de database succesvol een insert heeft uitgevoerd.*/
 
-        $gegevens["melding"] = "Uw account is geregistreerd. Klik op de onderstaande link in te loggen.";
+        $gegevens["melding"] = "Uw account is geregistreerd. <br> Klik op onderstaande knop om in te loggen.";
 
     else $gegevens["melding"] = "Het registreren is mislukt. Probeer het nog eens."; //anders word er onderstaande foutmelding gegeven.
 
     return $gegevens;
 }
-//de functie account registreren zal de gegevens van de klant opslaan in de database
-function accountregistreren($emailadres, $voornaam, $achternaam, $geslacht, $wachtwoord, $adres, $woonplaats, $postcode, $conn) {
+
+//de functie accountregistreren zal de gegevens van de klant opslaan in de database
+function accountregistreren($emailadres, $voornaam, $achternaam, $geslacht, $wachtwoord, $adres, $woonplaats, $postcode, $geboortedatum, $conn) {
     //met een INSERT voegen we de nieuwe klantgegevens toe aan de database
-    $sql = "INSERT INTO gebruiker (emailadres, voornaam, achternaam, geslacht, wachtwoord, adres, woonplaats, postcode) 
-            VALUES('$emailadres','$voornaam', '$achternaam', '$geslacht', '$wachtwoord','$adres','$woonplaats','$postcode')";
+    $sql = "INSERT INTO gebruiker (emailadres, voornaam, achternaam, geslacht, wachtwoord, adres, woonplaats, postcode, geboortedatum) 
+            VALUES('$emailadres','$voornaam', '$achternaam', '$geslacht', '$wachtwoord','$adres','$woonplaats','$postcode','$geboortedatum')";
 
-    $conn->query($sql);
+    $result = $conn->query($sql);
 
-    return $sql;
+    return $result;
 }
 
 //variabelen definieren
@@ -38,12 +41,12 @@ $emailadres = "emailadres";
 $voornaam = "voornaam";
 $achternaam = "achternaam";
 $geslacht = "geslacht";
-$geboortedatum = "geboortedatum";
 $wachtwoord = "wachtwoord";
 //$wachtwoord2 = "wachtwoord2";
 $adres = "adres";
 $woonplaats = "woonplaats";
 $postcode = "postcode";
+$geboortedatum = "geboortedatum";
 $message = "melding";
 
 // Als de knop "registreren" is geklikt, haal met $_GET de gegevens op.
@@ -52,25 +55,25 @@ if (isset($_GET["registreren"])){
     $gegevens[$voornaam] = isset($_GET[$voornaam]) ? $_GET[$voornaam] : "";
     $gegevens[$achternaam] = isset($_GET[$achternaam]) ? $_GET[$achternaam] : "";
     $gegevens[$geslacht] = isset($_GET[$geslacht]) ? $_GET[$geslacht] : "";
-    $gegevens[$geboortedatum] = isset($_GET[$geboortedatum]) ? $_GET[$geboortedatum] : "";
     $gegevens[$wachtwoord] = isset($_GET[$wachtwoord]) ? $_GET[$wachtwoord] : "";
     //$gegevens[$wachtwoord2] = isset($_GET[$wachtwoord2]) ? $_GET[$wachtwoord2] : "";
     $gegevens[$adres] = isset($_GET[$adres]) ? $_GET[$adres] : "";
     $gegevens[$woonplaats] = isset($_GET[$woonplaats]) ? $_GET[$woonplaats] : "";
     $gegevens[$postcode] = isset($_GET[$postcode]) ? $_GET[$postcode] : "";
+    $gegevens[$geboortedatum] = isset($_GET[$geboortedatum]) ? $_GET[$geboortedatum] : "";
     $gegevens = registreren($gegevens);
 }else{
-    //als het een veld niet is ingevult:
+    //als een veld niet is ingevult:
     $gegevens[$emailadres] = "";
     $gegevens[$voornaam] = "";
     $gegevens[$achternaam] = "";
     $gegevens[$geslacht] = "";
-    $gegevens[$geboortedatum] = "";
     $gegevens[$wachtwoord] = "";
     //$gegevens[$wachtwoord2] = "";
     $gegevens[$adres] = "";
     $gegevens[$woonplaats] = "";
     $gegevens[$postcode] = "";
+    $gegevens[$geboortedatum] = "";
     $gegevens[$message] = "";
 }
 
@@ -87,6 +90,8 @@ if (isset($_GET["registreren"])){
 
 <!-- Selfhandling formulier weergeven op het scherm, zodat de klant zijn of haar gegevens kan registreren. -->
 
+    <input type="text" name="emailadres" class="form-control "value="<?php print($gegevens[$emailadres]); ?>" placeholder="E-mail adres"/>
+    <br>
     <input type="text" name="voornaam" class="form-control "value="<?php print($gegevens[$voornaam]); ?>" placeholder="Voornaam"/>
     <br>
     <input type="text" name="achternaam" class="form-control "value="<?php print($gegevens[$achternaam]); ?>" placeholder="Achternaam"/>
@@ -94,8 +99,8 @@ if (isset($_GET["registreren"])){
     <input type="text" name="geslacht" class="form-control "value="<?php print($gegevens[$geslacht]); ?>" placeholder="Geslacht"/>
     <br>
     <input type="password" name="wachtwoord" class="form-control" value="<?php print($gegevens[$wachtwoord]); ?>" placeholder="Wachtwoord">
-    <br>
-   <!-- <input type="password" name="wachtwoord2" class="form-control" value="<?php print($gegevens[$wachtwoord2]); ?>" placeholder="Herhaal uw wachtwoord"> -->
+
+   <!-- <input type="password" name="wachtwoord2" class="form-control" value="<?php//print($gegevens[$wachtwoord2]); ?>" placeholder="Herhaal uw wachtwoord"> -->
     <br>
     <input type="text" name="adres" class="form-control" value="<?php print($gegevens[$adres]); ?>" placeholder="Adres"/>
     <br>
@@ -103,9 +108,18 @@ if (isset($_GET["registreren"])){
     <br>
     <input type="text" name="postcode" class="form-control" value="<?php print($gegevens[$postcode]); ?>" placeholder="Postcode"/>
     <br>
+    <input type="text" name="geboortedatum" class="form-control "value="<?php print($gegevens[$geboortedatum]); ?>" placeholder="Geboortedatum yyyy-mm-dd (optioneel)"/>
+    <br>
     <input type="submit" class="btn btn-outline-primary" name="registreren" value="registreren"/>
 </form>
-<br><?php print($gegevens[$message]); ?><br>
+<?php
+print($gegevens[$message]);
+
+?><br>
 <a class="btn btn-primary" href="inloggen.php" role="button">Terug naar de inlogpagina</a>
 </body>
 </html>
+<?php
+/*Nog te ontwikkelen; het veld wachtwoord bevestigen. Deze word niet opgenomen in de database (wat mij goed lijkt), maar ik heb
+nog geen functie gemaakt die controleerd of wachtwoord1 === wachtwoord2. */
+?>
