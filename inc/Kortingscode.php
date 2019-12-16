69e2376a20078540a0921cfed3f96ce59b60edb5
1147963 <s1147963@student.windesheim.nl>
2019-12-16 11:25:08 +0100

    16-12

--- inc/Kortingscode.php
+++ inc/Kortingscode.php
@@ -1,52 +1,78 @@
 <?php
 //functie work in progress
-function kortingsCodeToepassen($kortingscode,$winkelmandid,$kortingbestaat){ //deze functie past een kortingscode toe en berekend een nieuwe totaalprijs
 
-    //hieronder bouwen we de query naar de database op, met daarin de parameters op de juiste plekken.
-    $sql1 = "SELECT percentage FROM korting WHERE kortingscode = ".$kortingscode.";"; //het kortingspercentage ophalen
-    $sql2 = "SELECT totaalprijs FROM winkelmand WHERE winkelmand_id = ".$winkelmandid.";"; //de totaalprijs van de winkelmand ophalen
 
-    // hieronder voeren we de query uit
-    $oudeprijs = $conn->query($sql2); //de totaalprijs van de winkelmand
-    $kortingspercentage = $conn->query($sql1); //het kortingspercentage
+//database verbinding
+$conn = new mysqli('localhost','root','','world_wide_importers');
 
-    $nieuweprijs = 0; //de nieuweprijs voor het totaal
-    if(is_null($kortingscode )){ //geen kortingscode, dan blijft de oude prijs gelden
-        $nieuweprijs = $oudeprijs;
+function kortingsCodeBestaat($kortingscode, $conn){ //functie die controleerd of code bestaat
+    $sql = "SELECT korting_id FROM korting WHERE kortingscode =" . $kortingscode . ";";
+    $result = $conn->query($sql);
+    $codebestaat = 0;
+    if($result->num_rows == 0) {
+        $codebestaat = 0;// code bestaat niet
+    } else {
+        $codebestaat = 1;// code bestaat
     }
-    Elseif($kortingbestaat == False){ //de korting wordt berkeend
-        $nieuweprijs = (1-($kortingspercentage / 100)) * $oudeprijs; //kortingspercentage in procenten naar nieuwe prijs
-    }
-    else{$nieuweprijs = $oudeprijs;
-    }
+    return $codebestaat;
+}
 
-    $sql4 = "UPDATE winkelmand SET totaalprijs = ".$nieuweprijs."WHERE winkelmand_id = ".$winkelmandid.";"; //schrijft de nieuwe prijs naar de database
+function alKortingsCodeGebruikt($winkelmandid,$codebestaat,$conn){//deze functie controleert of een kortingscode al reeds is toegepast true/false
+    $sql = "SELECT kortingscode FROM winkelmand WHERE winkelmand_id = ".$winkelmandid.";";
+    $result = $conn->query($sql);
+    if($codebestaat == 1){
+    if(is_null($result)){ //Er is nog geen kortingscode ingevoerd
+        $kortingalgebruikt = 0;
+    }
+    else{
+        $kortingalgebruikt = 1; // je mag maar 1 code toepassen
+    }
+    }
+    else{
+        $kortingalgebruikt = 2; //er is geen bestaande code ingevoerd
+    }
+    return $kortingalgebruikt;
+}
+
+function kortingsCodeToepassen($kortingscode,$winkelmandid,$conn){ //deze functie past een kortingscode toe en berekend een nieuwe totaalprijs
+
+    $codebestaat1 = kortingsCodeBestaat($kortingscode,$conn);
+    $kortingalgebruikt1 = alKortingsCodeGebruikt($winkelmandid,$codebestaat1,$conn);
+
+    $sql1 = "SELECT totaalprijs FROM winkelmand WHERE winkelmand_id = " . $winkelmandid . ";"; //de totaalprijs van de winkelmand ophalen
+    $result1 = $conn->query($sql1); //de totaalprijs van de winkelmand $oudeprijs
+
+    if($codebestaat1 = 1) {//de code bestaat
+        //hieronder bouwen we de query naar de database op, met daarin de parameters op de juiste plekken.
+        $sql2 = "SELECT percentage FROM korting WHERE kortingscode = " . $kortingscode . ";"; //het kortingspercentage ophalen
+        // hieronder voeren we de query uit
+        $result2 = $conn->query($sql2); //het kortingspercentage $kortingspercentage
+
+        if ($kortingalgebruikt1 == 0) { //als er nog geen korting is gebruikt dan wordt de korting berekend
+            $nieuweprijs = (1 - ($result2 / 100)) * $result1; //kortingspercentage in procenten naar nieuwe prijs
+            $sql3 = "UPDATE winkelmand SET totaalprijs = ".$nieuweprijs."WHERE winkelmand_id = ".$winkelmandid.";"; //schrijft de nieuwe prijs naar de database
+            $conn->query($sql3);
+        } else {
+            $nieuweprijs = $result1; //geen wijziging in nieuweprijs
+        }
+    }
+    else{ //de totaalprijs van de winkelwagen wordt opgehaald
+        $nieuweprijs = $result1;
+    }
     return $nieuweprijs;
 }
 
-function alKortingsCodeGebruikt($winkelmandid){//deze functie controleert of een kortingscode al reeds is toegepast true/false
-    $sql = "SELECT kortingscode FROM winkelmand WHERE winkelmand_id = ".$winkelmandid.";"; //het kortingspercentage ophalen
-    if(is_null($sql)){ //Er is nog geen kortingscode ingevoerd
-        $kortingbestaat = false;
-    }
-    else{
-        $kortingbestaat = true; // je mag maar 1 code toepassen
-    }
-    return $kortingbestaat;
+function kortingsCodeVerwijderen($winkelmandid, $conn){ //deze functie verwijderd de (alle) kortingscode
+    $sql = "UPDATE winkelmand SET kortingscode = NULL WHERE winkelmand_id = ".$winkelmandid.";"; // kortingscode veld leegmaken
+    $result = $conn->query($sql);
+    return mysqli_stmt_affected_rows($result) == 1;
 }
 
-function kortingsCodeVerwijderen($winkelmandid){ //deze functie verwijderd de (alle) kortingscode
-    $sql = "UPDATE winkelmand SET kortingscode IS NULL WHERE winkelmand_id = ".$winkelmandid.";"; //het kortingspercentage ophalen
-    return mysqli_stmt_affected_rows($sql) == 1;
+function kortingsNaamTonen($kortingscode, $conn){ //functie geeft de naam terug van een kortingscode
+    $sql = "SELECT kortingnaam FROM korting WHERE kortingscode = ".$kortingscode.";"; //de naam van de korting ophalen
+    $result = $conn->query($sql);
+    return $result;
 }
-
-function kortingsNaamTonen($kortingscode){ //functie geeft de naam terug van een kortingscode
-    $sql = "SELECT kortingnaam FROM korting WHERE kortingscode = ".$kortingscode.";"; //het kortingspercentage ophalen
-    $kortingsnaam = $conn->query($sql); //de naam van de korting
-    return $kortingsnaam;
-}
-
 ?>
 
 
-//wat nog ontbreekt: wat als code al eens gebruikt is dit moet opgeslagen worden.
--- inc/besteloverzicht.php
+++ inc/besteloverzicht.php
@@ -1,3 +1,26 @@
+<?php
+include "head.php";
+include "footer.php";
+include "menu.php";
+include "Kortingscode.php";
+
+//database verbinding
+$conn = new mysqli('localhost','root','','world_wide_importers');
+
+//variabelen aanmaken voor deze pagina
+$kortingscode = 'placeholder';
+
+if (isset($_GET["Toepassen"])){ //kortingscode ophalen uit formulier
+    $kortingscode = isset($_GET["kortingscode"]);
+    $prijs = kortingsCodeToepassen($kortingscode,'1',$conn); //$winkelmandid is hier nog ff 1
+}
+
+if (isset($_GET["Verwijder+code"])){ //kortingscode verwijderen
+    $kortingscode = isset($_GET["kortingscode"]);
+    kortingsCodeVerwijderen('1', $conn); //$winkelmandid is hier nog ff 1
+}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -17,26 +40,34 @@
 
 <div class="container">
     <h3>hier komt de totaalprijs van de winkelwagen</h3><br>
+    <?php
+    if(isset($prijs)){echo $prijs;} //als een prijs is dan deze tonen
+    ?>
 </div>
 
 <div class="container">
     <label for="kortingscodeinput">Een kortingscode toepassen?</label>
-    <form action ="/Kortingscode.php" method="get" class="form-inline">
+    <form action ="besteloverzicht.php" method="get" class="form-inline">
         <div class="form-group">
-        <input type="text" name="kortingscode" class="form-control" placeholder="typ hier de kortingscode">
-        <input type="submit" name="Korting toepassen" value="Toepassen" class="btn btn-primary">
+        <input type="text" name="kortingscode" class="form-control" placeholder="typ hier de kortingscode">&nbsp
+        <input type="submit" name="Kortingtoepassen" value="Toepassen" class="btn btn-primary">&nbsp
+            <input type="submit" name="Kortingverwijderen" value="Verwijder code" class="btn btn-primary">
         </div>
     </form>
     <small id="kortingantwoord" class="form-text text-muted">Hier komt feedback over de kortingscode</small>
     <br>
+    <?php
+
+    ?>
     <form method="get">
         <div class="form-group">
             <label for="kortingscodeinput">Wil je de bestelling plaatsen?</label><br>
-        <input type="submit" name="bestellen" value="Plaats bestelling" class="btn btn-primary"><br>
+            <input type="submit" name="bestellen" value="Plaats bestelling" class="btn btn-primary"><br>
         </div>
     </form>
 </div>
 <br>
+
 <div class="container">
     <button onclick="history.go(-1);" class="btn btn-primary">Terug </button>
 </div>
