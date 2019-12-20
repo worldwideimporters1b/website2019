d4b087ec9dfb20d6402a0aafda1a8cb5208da5cc
Lena <s1003879>
2019-12-20 10:58:30 +0100

    Merge branch 'master' of C:\Users\Lena\PhpstormProjects\website2019 with conflicts.

--- inc/checkout.php
+++ inc/checkout.php
@@ -1,20 +1,19 @@
 <?php
 
-function pay($amount, $orderid, $customerid)
-{
+function pay($amount, $orderid, $customerid ){
 
     $paymentresult = 0;
 
     // render ideal stuff on website in iframe
 
-    $idealresult = renderIdeal($amount, $shopid, $orderid);
+    $idealresult = renderIdeal($amount,$shopid,$orderid);
 
     // check payment
 
-    if ($idealresult == 0) {
+    if($idealresult == 0){
         $paymentresult = 0;
     }
-    if ($idealresult == 1) {
+    if($idealresult == 1){
         $paymentresult = 1;
     }
 
@@ -25,22 +24,21 @@ function pay($amount, $orderid, $customerid)
 }
 
 
-function renderIdeal($amount, $shopid, $orderid){
-$idealresult = 0;
 
+function renderIdeal($amount,$shopid,$orderid){
+$idealresult = 0;
+}
 // do something
 
 return $idealresult;
 }
-$idealresult = 1;
-if ($idealresult == 0) {
+
+if($idealresult == 0){
     $paymentresult = 0;
 }
-if ($idealresult == 1) {
+if($idealresult == 1){
     $paymentresult = 1;
 }
-include('head.php');
-include('header.php');
-echo "<img src='../img/ideal.png'/>";
+
 
 ?>
