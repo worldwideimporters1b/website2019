5d462e34d02f1ac2ede2614381e1b56f7627f2db
Lennard <s1080997@student.windesheim.nl>
2019-12-21 21:39:17 +0100

    Accountverwijderen functies werken!

--- inc/accountverwijderen.php
+++ inc/accountverwijderen.php
@@ -4,8 +4,7 @@
 @include "head.php";
 @include "header.php";
 
-// require_once "accountwijzigen.php";
-
+//verbinding met de database maken
 $conn = new mysqli('localhost','root','','world_wide_importers');
 
 function controle($emailadres, $wachtwoord, $conn){
@@ -13,12 +12,17 @@ function controle($emailadres, $wachtwoord, $conn){
     $query = "SELECT COUNT(*) FROM gebruiker WHERE `emailadres` = '$emailadres' AND `wachtwoord` = '$wachtwoord';"; //controleren of er een emailadres is met het ingevoerde wachtwoord
 
     $result = $conn->query($query);     //Wanneer de query word uitgevoerd, komt er 0 of 1 uit als resultaat.
+    $row = $result->fetch_row();        // Het resultaat komt in de vorm van een array. Deze array slaan we op in $row
 
-    if ($result === 1) { //als het niet gelijk is aan 0, voeren we de verwijdering van het account uit.
+    if ($row[0] == 1) {                 //Op plek 0 van de array $row staat een 0 of 1. 0 betekend dat het emailadres of wachtwoord onjuist is.
+                                        //Als plek 0 van $row een 1 heeft als waarde, is het emailadres en wachtwoord een match. Het account mag verwijderd worden.
         accountVerwijderen($emailadres, $conn);
+
     }else{
-        echo "E-mailadres of wachtwoord is onjuist.";
+       echo "E-mailadres of wachtwoord is onjuist."; //feedback geven dat $row[0] == 0, oftewel emailadres en wachtwoord matchen niet.
+
     }
+
     return $result;
 }
 
@@ -26,12 +30,12 @@ function accountVerwijderen($emailadres, $conn){
     // $sql = "DELETE FROM gebruiker WHERE `gebruiker_id` = ".$gebruikersid.";";
     $sql = "DELETE FROM gebruiker WHERE `emailadres` = '$emailadres';"; //account verwijderen op basis van het ingevoerde emailadres.
 
-    $resultaat = $conn->query($sql);
+    $result = $conn->query($sql);
 
-    if($resultaat === TRUE) { //feedback geven
+    if($result === TRUE) { //feedback geven
         echo "Uw account is verwijderd.";
     }
-    return $resultaat;
+    return $result;
 }
 
 if(isset($_POST["verwijderen"])){ //wanneer de knop "verwijderen" is geklikt;
