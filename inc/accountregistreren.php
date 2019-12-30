0090c3cd2b5bba1029ef0488d0fdfc5c9d6d4d5d
Lennard <s1080997@student.windesheim.nl>
2019-12-30 14:23:55 +0100

    Accountregistratie email+wachtwoord controle toegevoegd

--- inc/accountregistreren.php
+++ inc/accountregistreren.php
@@ -14,16 +14,23 @@ function databaseConnectie(){
 //deze functie zal de de registratie functie aanroepen, en controleren of het account is geregistreerd in de database.
 function registreren($gegevens) {
     $conn = databaseConnectie();
-    if($gegevens["wachtwoord"])
-    if (accountregistreren($gegevens["emailadres"], $gegevens["voornaam"],$gegevens["achternaam"], $gegevens["geslacht"],$gegevens["wachtwoord"],
-            $gegevens["adres"],$gegevens["woonplaats"], $gegevens["postcode"], $gegevens["geboortedatum"], $conn) == 1)
+
+    if (accountregistreren($gegevens['emailadres'], $gegevens["voornaam"],$gegevens["achternaam"], $gegevens["geslacht"],$gegevens["wachtwoord"],
+            $gegevens["adres"],$gegevens["woonplaats"], $gegevens["postcode"], $gegevens["geboortedatum"], $conn) == 1) {
 
         /*controle of het account succesvol is geregistreerd. Wanneer de webshop 1 terug geeft, is het gelukt.
            $result geeft een 1 terug in de functie accountRegistreren, als de database succesvol een insert heeft uitgevoerd.*/
 
-        $gegevens["melding"] = "Uw account is geregistreerd. <br> Klik op onderstaande knop om in te loggen.<br>";
+        echo "<blockquote class=\"blockquote text-center\">";
+        echo "<p class=\"mb-0\"><strong>Uw account is geregistreerd. <br> Klik op onderstaande knop om in te loggen.<br></p></strong>";
+        echo "</blockquote>";
+    }
 
-    else $gegevens["melding"] = "Het registreren is mislukt. Probeer het nog eens."; //anders word er onderstaande foutmelding gegeven.
+    else {
+        echo "<blockquote class=\"blockquote text-center\">";
+        echo "<p class=\"mb-0\"><strong>Het registreren is mislukt. Probeer het nog eens.</strong></p>"; //anders word er onderstaande foutmelding gegeven.
+        echo "</blockquote>";
+    }
 
     return $gegevens;
 }
@@ -40,51 +47,53 @@ function accountregistreren($emailadres, $voornaam, $achternaam, $geslacht, $wac
 }
 
 //variabelen definieren
-$emailadres = "emailadres";
 $voornaam = "voornaam";
 $achternaam = "achternaam";
 $geslacht = "geslacht";
 $wachtwoord = "wachtwoord";
-//$wachtwoord2 = "wachtwoord2";
 $adres = "adres";
 $woonplaats = "woonplaats";
 $postcode = "postcode";
 $geboortedatum = "geboortedatum";
 $message = "melding";
 
-/*if(isset($_POST["registreren"])) { //wachtwoordcontrole (?)
-    if (strlen(($_POST[$wachtwoord])) < 6 OR strlen(($_POST[$wachtwoord])) > 20 OR !preg_match("#[0-9]+#", ($_POST[$wachtwoord])) OR !preg_match("#[a-z]+#", ($_POST[$wachtwoord]))) {
-        print("tea");
-    }
-}*/
 
 // Als de knop "registreren" is geklikt, haal met $_POST de gegevens op.
-if (isset($_POST["registreren"])){
-    $gegevens[$emailadres] = isset($_POST[$emailadres]) ? $_POST[$emailadres] : "";
-    $gegevens[$voornaam] = isset($_POST[$voornaam]) ? $_POST[$voornaam] : "";
-    $gegevens[$achternaam] = isset($_POST[$achternaam]) ? $_POST[$achternaam] : "";
-    $gegevens[$geslacht] = isset($_POST[$geslacht]) ? $_POST[$geslacht] : "";
-    $gegevens[$wachtwoord] = isset($_POST[$wachtwoord]) ?  md5("a@sdiu#(*$1_41" . $_POST[$wachtwoord]) : ""; //md5 encryptie om het wachtwoord versleutelt op te slaan.
-    //$gegevens[$wachtwoord2] = isset($_POST[$wachtwoord2]) ? $_POST[$wachtwoord2] : "";
-    $gegevens[$adres] = isset($_POST[$adres]) ? $_POST[$adres] : "";
-    $gegevens[$woonplaats] = isset($_POST[$woonplaats]) ? $_POST[$woonplaats] : "";
-    $gegevens[$postcode] = isset($_POST[$postcode]) ? $_POST[$postcode] : "";
-    $gegevens[$geboortedatum] = isset($_POST[$geboortedatum]) ? $_POST[$geboortedatum] : "";
-    $gegevens = registreren($gegevens);
-}else{
-    //als een veld niet is ingevult:
-    $gegevens[$emailadres] = "";
-    $gegevens[$voornaam] = "";
-    $gegevens[$achternaam] = "";
-    $gegevens[$geslacht] = "";
-    $gegevens[$wachtwoord] = "";
-    //$gegevens[$wachtwoord2] = "";
-    $gegevens[$adres] = "";
-    $gegevens[$woonplaats] = "";
-    $gegevens[$postcode] = "";
-    $gegevens[$geboortedatum] = "";
-    $gegevens[$message] = "";
+if (isset($_POST['registreren'])) {
+    if (strlen(($_POST[$wachtwoord])) < 6 || strlen(($_POST[$wachtwoord])) > 20 || !preg_match('@[A-Z]@', ($_POST[$wachtwoord])) || !preg_match('@[a-z]@', ($_POST[$wachtwoord]))
+        || !preg_match('@[^\w]@', ($_POST[$wachtwoord]))) {
+        echo "Het wachtwoord moet minimaal 6  en maximaal 20 tekens bevatten. Het wachtwoord moet bestaan uit een normale en hoofdletter. Het wachtwoord moet minstens 1 speciaal karakter bevatten.";
+    }else {
+            $emailadres = $_POST['emailadres'];
+            $conn = databaseConnectie();
+            $sql = "SELECT COUNT(*) FROM gebruiker WHERE `emailadres` = '$emailadres';";  //Met deze sql query controleren we of het emailadres al in gebruik is.
+            $result = $conn->query($sql);           //Wanneer de query word uitgevoerd, komt er 0 of 1 uit als resultaat.
+            $row = $result->fetch_row();             // Het resultaat komt in de vorm van een array. Deze array slaan we op in $row
+
+            if ($row[0] == 0) {                 //Op plek 0 van de array $row staat een 0 of 1. 0 betekend of het emailadres nog niet in gebruik is.
+                                                 //Als plek 0 van $row een 0 staat, mag het account gemaakt worden.
+
+                $gegevens['emailadres'] = $_POST['emailadres'];
+                $gegevens[$voornaam] = isset($_POST[$voornaam]) ? $_POST[$voornaam] : "";
+                $gegevens[$achternaam] = isset($_POST[$achternaam]) ? $_POST[$achternaam] : "";
+                $gegevens[$geslacht] = isset($_POST[$geslacht]) ? $_POST[$geslacht] : "";
+                $gegevens[$wachtwoord] = isset($_POST[$wachtwoord]) ? md5("a@sdiu#(*$1_41" . $_POST[$wachtwoord]) : ""; //md5 encryptie om het wachtwoord versleutelt op te slaan.
+                $gegevens[$adres] = isset($_POST[$adres]) ? $_POST[$adres] : "";
+                $gegevens[$woonplaats] = isset($_POST[$woonplaats]) ? $_POST[$woonplaats] : "";
+                $gegevens[$postcode] = isset($_POST[$postcode]) ? $_POST[$postcode] : "";
+                $gegevens[$geboortedatum] = isset($_POST[$geboortedatum]) ? $_POST[$geboortedatum] : "";
+                $gegevens = registreren($gegevens);
+
+            } else {
+                echo "<blockquote class=\"blockquote text-center\">";
+                echo "<p class=\"mb-0\"><strong>Dit emailadres is al in gebruik</p></strong>";
+                echo "</blockquote>";
+            }
+
+        }
+
 }
+
 
 ?>
 
@@ -95,38 +104,38 @@ if (isset($_POST["registreren"])){
 
 <!-- Selfhandling formulier weergeven op het scherm, zodat de klant zijn of haar gegevens kan registreren. Alle velden zijn verplicht, op geslacht en geb. datum na. -->
 
-    <input type="email" name="emailadres" class="form-control "value="<?php print($gegevens[$emailadres]); ?>" placeholder="E-mail adres" required/>
+    <input type="email" name="emailadres" class="form-control "value="<?php
+    if(isset($_POST['registreren'])){ echo $_POST['emailadres']; }?>" placeholder="E-mail adres" required/>
     <br>
-    <input type="text" name="voornaam" class="form-control "value="<?php print($gegevens[$voornaam]); ?>" placeholder="Voornaam" required/>
+    <input type="text" name="voornaam" class="form-control "value="<?php
+    if(isset($_POST['registreren'])){ echo $_POST['voornaam']; }?>" placeholder="Voornaam" required/>
     <br>
-    <input type="text" name="achternaam" class="form-control "value="<?php print($gegevens[$achternaam]); ?>" placeholder="Achternaam"required/>
+    <input type="text" name="achternaam" class="form-control "value="<?php
+    if(isset($_POST['registreren'])){ echo $_POST['achternaam']; }?>" placeholder="Achternaam"required/>
     <br>
-    <input type="text" name="geslacht" class="form-control "value="<?php print($gegevens[$geslacht]); ?>" placeholder="Geslacht (optioneel)"/>
+    <input type="text" name="geslacht" class="form-control "value="<?php
+    if(isset($_POST['registreren'])){ echo $_POST['geslacht']; }?>" placeholder="Geslacht (optioneel)"/>
     <br>
-    <input type="password" name="wachtwoord" class="form-control" value="<?php print($gegevens[$wachtwoord]); ?>" placeholder="Wachtwoord" required>
-
-   <!-- <input type="password" name="wachtwoord2" class="form-control" value="<?php//print($gegevens[$wachtwoord2]); ?>" placeholder="Herhaal uw wachtwoord"> -->
+    <input type="password" name="wachtwoord" class="form-control" value="<?php
+    if(isset($_POST['registreren'])){ echo $_POST['wachtwoord']; }?>" placeholder="Wachtwoord" required>
     <br>
-    <input type="text" name="adres" class="form-control" value="<?php print($gegevens[$adres]); ?>" placeholder="Adres" required/>
+    <input type="text" name="adres" class="form-control" value="<?php
+    if(isset($_POST['registreren'])){ echo $_POST['adres']; }?>" placeholder="Adres" required/>
     <br>
-    <input type="text" name="woonplaats" class="form-control" value="<?php print($gegevens[$woonplaats]); ?>" placeholder="Woonplaats" required/>
+    <input type="text" name="woonplaats" class="form-control" value="<?php
+    if(isset($_POST['registreren'])){ echo $_POST['woonplaats']; }?>" placeholder="Woonplaats" required/>
     <br>
-    <input type="text" name="postcode" class="form-control" value="<?php print($gegevens[$postcode]); ?>" placeholder="Postcode" required/>
+    <input type="text" name="postcode" class="form-control" value="<?php
+    if(isset($_POST['registreren'])){ echo $_POST['postcode']; }?>" placeholder="Postcode" required/>
     <br>
-    <input type="text" name="geboortedatum" class="form-control "value="<?php print($gegevens[$geboortedatum]); ?>" placeholder="Geboortedatum yyyy-mm-dd (optioneel)"/>
+    <input type="text" name="geboortedatum" class="form-control "value="<?php
+    if(isset($_POST['registreren'])){ echo $_POST['geboortedatum']; }?>" placeholder="Geboortedatum yyyy-mm-dd (optioneel)"/>
     <br>
     <input type="submit" class="btn btn-outline-success" name="registreren" value="registreren"/>
 </form>
 </div>
 <div class="container">
-<?php
-print($gegevens[$message]);
-
-?><br>
+<br>
 <a class="btn btn-outline-primary" href="inloggen.php" role="button">Terug naar de inlogpagina</a>
 </div>
 
-<?php
-/*Nog te ontwikkelen; het veld wachtwoord bevestigen. Deze word niet opgenomen in de database (wat mij goed lijkt), maar ik heb
-nog geen functie gemaakt die controleerd of wachtwoord1 === wachtwoord2. */
-?>
