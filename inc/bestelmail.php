90b284a0ccf7627e67845f61ee0047f9ccde63b4
1147963 <s1147963@student.windesheim.nl>
2020-01-08 20:37:43 +0100

    bestelmail

---
+++ inc/bestelmail.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<?php
+include "head.php";
+
+?>
+<div class="container">
+    <br>
+    <h3> Beste Theo, Bedankt voor uw bestelling</h3>
+    <br>
+    <h5> Je besteling wordt momenteel gesorteerd en zo spoedig mogelijk naar jouw adres verstuurd. </h5>
+    <br>
+    <table style="width:100%" class="table">
+        <tr>
+            <th>Producten</th>
+            <th>Verwachte verzenddatum</th>
+            <th>Aantal</th>
+            <th>Prijs</th>
+            <th>Verzendwijze</th>
+        </tr>
+        <tr>
+            <td>USB rocketlauncher </td>
+            <td>03-02-2020</td>
+            <td>2</td>
+            <td>€25 </td>
+            <td>DHL </td>
+        </tr>
+    </table>
+
+    <h6> Het totaalbedrag inclusief verzendkosten: €28</h6>
+    <br>
+    <table style="width:50%" class="table">
+        <tr>
+            <th>Ordernummer:</th>
+            <th>Klantnummer:</th>
+            <th>Besteldatum:</th>
+            <th>Betaalwijze:</th>
+
+        </tr>
+        <tr>
+            <td>SO36232688</td>
+            <td>774</td>
+            <td>5 december 2019</td>
+            <td>IDEAL </td>
+        </tr>
+    </table>
+    <br>
+    <h5>Afleveradres:</h5>
+    <h6>T. Arrél </h6>
+    <h6>Dixilaan 69 </h6>
+    <h6>1787 AZ Den Helder </h6>
+    <br>
+    <h5>Factuuradres:</h5>
+    <h6>T. Arrél </h6>
+    <h6>Dixilaan 69 </h6>
+    <h6>1787 AZ Den Helder </h6>
+    <br>
+    <h4> Veel plezier met je bestelling!  </h4>
+    <h4> Het WWI team!</h4>
+</div>
