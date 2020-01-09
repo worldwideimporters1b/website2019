32970f2b3e57252ce1ee69a3952cc3cd851e9138
sander.brandriet <sanderbrandriet@gmail.com>
2020-01-09 11:11:44 +0100

    Kleine updates

--- inc/basket.php
+++ inc/basket.php
@@ -10,8 +10,7 @@ echo toonWinkelmand(basketinfo($conn), $conn);
 echo '
 <form method="get" target="accountoverzicht.php">
         <div class="form-group">
-            <label for="kortingscodeinput">Doorgaan?</label><br>
-            <a class="btn btn-secondary" href="productpagina.php" role="button">Verder winkelen</a> <a class="btn btn-secondary" href="accountoverzicht.php" role="button">Inloggen & verzendinformatie</a>
+            <a class="btn btn-secondary" href="productpagina.php" role="button">Verder winkelen</a> <a class="btn btn-secondary" href="accountoverzicht.php" role="button">Account & Verzendinformatie</a>
         </div>
     </form>';
 echo "</div>";
