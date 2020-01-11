5de9371f726a3d7509ee11584a1deee5f147b3ad
Lennard <s1080997@student.windesheim.nl>
2020-01-11 18:58:38 +0100

    Contactform. dynamisch

--- inc/contact.php
+++ inc/contact.php
@@ -1,14 +1,7 @@
-<!doctype html>
-
 <?php
-
-
+include 'head.php';
+include 'header.php';
 ?>
-
-<?php include 'head.php'; ?>
-
-<?php include 'header.php' ?>
-
 <body>
 <div class="container">
     <h2 class="h1-responsive font-weight-bold text-center my-4">Contact</h2>
@@ -21,20 +14,71 @@
         <div class="col-md-9 mb-md-0 mb-5">
             <form id="contact-form" name="contact-form" action="contact.php" method="POST">
                 <div class="row">
+                    <?php if (!isset($_SESSION['ingelogd'])) {  //wanneer je niet bent ingelogd, verschijnt het email veld.
+                        ?>
                     <div class="col-md-12">
                         <div class="md-form mb-0">
-                            <input type="text" id="name" name="name" class="form-control">
+                            <input type="text" id="email" name="email" class="form-control" required>
+                            <label for="email" class="">Uw email</label>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="md-form mb-0">
+                            <input type="text" id="name" name="name" class="form-control" required>
                             <label for="name" class="">Uw naam</label>
                         </div>
                     </div>
-
-
                 </div>
 
                 <div class="row">
                     <div class="col-md-12">
                         <div class="md-form mb-0">
                             <input type="text" id="subject" name="subject" class="form-control">
+                            <label for="subject" class="">Onderwerp</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="md-form">
+                            <textarea type="text" id="message" name="message" rows="2"
+                                      class="form-control md-textarea"></textarea>
+                            <label for="message">Bericht</label>
+                        </div>
+
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Verstuur</button>
+            </form>
+            <div class="status"></div>
+        </div>
+
+        <div class="col-md-3 text-center">
+            <ul class="list-unstyled mb-0">
+                <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                    <p>Nederland, 1111AA , Zwolle</p>
+                </li>
+
+                <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                    <p>+31 6 123 456 78</p>
+                </li>
+
+                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                    <p>contact@worldwideimporters.nl</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+                    <?php } //anders hoef je geen emailadres en naam in te voeren.
+                    else{ ?>
+</div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="md-form mb-0">
+                            <input type="text" id="subject" name="subject" class="form-control" required>
                             <label for="subject" class="">Onderwerp</label>
                         </div>
                     </div>
@@ -74,6 +118,7 @@
         </div>
 
     </div>
+    <?php } ?>
 </div>
 </body>
 
