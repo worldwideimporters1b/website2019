815caed30c4eea5f990aa8e738017dcaedd17ada
sander.brandriet <sanderbrandriet@gmail.com>
2020-01-12 17:24:04 +0100

    besteloverzicht btw

--- inc/besteloverzicht.php
+++ inc/besteloverzicht.php
@@ -46,7 +46,7 @@ if (isset($_GET["Kortingverwijderen"])) { //kortingscode verwijderen
             <td>BTW</td>
             <td>
 
-                <?php echo formatprijs(BtwTonen($kortingscode, $winkelmandid, $conn)); ?>
+                <?php echo (BtwTonen($kortingscode, $winkelmandid, $conn); ?>
             </td>
             <!--            <td>€--><?php //echo BtwTonen($kortingscode, $winkelmandid, $conn); ?><!--</td>-->
         </tr>
